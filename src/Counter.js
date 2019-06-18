8d2899bb35a826b649f536613f1f5607b8d45e98
Bryan Nelson <32003096+coolsitebiz@users.noreply.github.com>
2019-06-17 17:05:23 -0700

    added mode switching at :00

--- src/Counter.js
+++ src/Counter.js
@@ -4,22 +4,32 @@ class Counter extends React.Component {
     constructor() {
         super();
         this.state = {
-            count: 1500,
-            running: false
-            
+            count: 10,
+            running: false,
+            mode: "Session",
+            normalLength: 120,
+            shortLength: 60
         }
         this.myVar = null;
     }
 
-    incrementCounter = () => {
-        this.setState({
-            count: this.state.count - 1
-        });
+    handleCounter = () => {
+        if (this.state.count > 1) {
+            this.setState({
+                count: this.state.count - 1
+            });
+        } else {
+            if (this.state.mode === "Session") {
+                this.setState({count: this.state.shortLength, mode: "Break"});
+            } else if (this.state.mode === "Break") {
+                this.setState({count: this.state.normalLength, mode: "Session"});
+            }
+        }
     }
 
     toggleTimer = () => {
         if(!this.state.running) {
-            this.myVar = setInterval(this.incrementCounter, 1000);
+            this.myVar = setInterval(this.handleCounter, 1000);
             this.setState({
                 running: true
             })
@@ -32,9 +42,34 @@ class Counter extends React.Component {
         }
     }
 
+    resetTimer = () => {
+        this.setState({
+            count: this.state.normalLength,
+            mode: "Session",
+            running: false
+        })
+        clearInterval(this.myVar);
+    }
+
+
+
     render() {
         return(
-            <div><button onClick={this.toggleTimer}>{Math.floor(this.state.count / 60)}:{this.state.count % 60}</button></div>
+            <div className="container">
+                <div>Break Length</div>
+                <div>{this.state.mode}</div>
+                <div>Session Length</div>
+                <div>{Math.floor(this.state.shortLength / 60)}</div>
+                <div></div>
+                <div>{Math.floor(this.state.normalLength / 60)}</div>
+                <div className="clockButton"  onClick={this.toggleTimer}>Start/Stop</div>
+                <div className="clockDisplay">
+                    {Math.floor(this.state.count / 60) < 10 ? "0" + Math.floor(this.state.count / 60) : Math.floor(this.state.count / 60)}
+                    :
+                    {this.state.count % 60 < 10 ? "0" + this.state.count % 60 : this.state.count % 60}
+                </div>
+                <div className="clockButton" onClick={this.resetTimer}>Reset</div>
+            </div>
         )
     }
 }
