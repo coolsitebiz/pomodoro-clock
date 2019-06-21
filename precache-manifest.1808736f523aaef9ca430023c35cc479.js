4a6b09bec0f3930239205dfa0b8f4c3ad3026736
Bryan Nelson <32003096+coolsitebiz@users.noreply.github.com>
2019-06-21 14:33:42 -0700

    Updates

---
+++ precache-manifest.1808736f523aaef9ca430023c35cc479.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec4bbf53e1c073c645bd670bda37557",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "ceb14c97bfef4f7df44b",
+    "url": "/pomodoro-clock/static/css/main.d4ed26e4.chunk.css"
+  },
+  {
+    "revision": "7e09af94520522384a08",
+    "url": "/pomodoro-clock/static/js/2.5e58f91e.chunk.js"
+  },
+  {
+    "revision": "ceb14c97bfef4f7df44b",
+    "url": "/pomodoro-clock/static/js/main.8da1f1b7.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  },
+  {
+    "revision": "72b919fbe2464718b1910f7a517c24b3",
+    "url": "/pomodoro-clock/static/media/BEEP.72b919fb.mp3"
+  }
+]);
